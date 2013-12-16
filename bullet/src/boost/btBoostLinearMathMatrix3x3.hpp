f41f64f3bafad2bbac10d5b38001d2908d371408
Högni Gylfason <klumhru@gmail.com>
2013-12-16 15:40:51 +0000

    btMatrix3x3 common items ready

---
+++ bullet/src/boost/btBoostLinearMathMatrix3x3.hpp
@@ -0,0 +1,65 @@
+// File: btBoostLinearMathMatrix3x3.hpp
+#ifndef _btBoostLinearMathMatrix3x3_hpp
+#define _btBoostLinearMathMatrix3x3_hpp
+
+#include <iostream>
+#include <boost/python.hpp>
+#include <LinearMath/btMatrix3x3.h>
+
+ostream& operator<<(ostream& os, const btMatrix3x3& m)
+{
+    btVector3 a = m.getColumn(0),
+        b = m.getColumn(1),
+        c = m.getColumn(2);
+    os << "("
+       << "(" << a.x() << ", " << a.y() << ", " << a.z() << "), "
+       << "(" << b.x() << ", " << b.y() << ", " << b.z() << "), "
+       << "(" << c.x() << ", " << c.y() << ", " << c.z() << "))";
+    return os;
+}
+
+btVector3& m3x3_get_item_mutable(btMatrix3x3& m, int i)
+{
+    return m[i];
+}
+
+btVector3& m3x3_set_item(btMatrix3x3& m, int i, const btVector3& v)
+{
+    return m[i] = v;
+}
+
+void defineMatrix3x3()
+{
+    class_<btMatrix3x3>("btMatrix3x3")
+        // Constructors
+        .def(init<const btQuaternion&>())
+        .def(init<const btScalar&, const btScalar&, const btScalar&,
+                  const btScalar&, const btScalar&, const btScalar&,
+                  const btScalar&, const btScalar&, const btScalar&>())
+        .def(init<const btMatrix3x3&>())
+        // String operators
+        .def(self_ns::str(self_ns::self))
+        .def(self_ns::repr(self_ns::self))
+        // Operators
+        .def("__getitem__", &m3x3_get_item_mutable,
+             return_value_policy<copy_non_const_reference>())
+        .def("__setitem__", &m3x3_set_item,
+             return_value_policy<copy_non_const_reference>())
+        .def(self *= other<btMatrix3x3>())
+        .def(self * other<btMatrix3x3>())
+        .def(self * other<btScalar>())
+        .def(self += other<btMatrix3x3>())
+        .def(self + other<btMatrix3x3>())
+        .def(self -= other<btMatrix3x3>())
+        .def(self - other<btMatrix3x3>())
+        .def(self == other<btMatrix3x3>())
+        // Member functions
+        .def("get_row", &btMatrix3x3::getRow,
+             return_value_policy<copy_const_reference>())
+        .def("get_column", &btMatrix3x3::getColumn,
+             return_value_policy<return_by_value>())
+
+        ;
+}
+
+#endif // _btBoostLinearMathMatrix3x3_hpp
---
+++ linear_math_tests/test_matrix3x3.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+linear_math_tests.test_matrix3x3
+"""
+from __future__ import unicode_literals, print_function, absolute_import
+
+import unittest
+import math
+
+import bullet
+
+
+class TestMatrix3x3(unittest.TestCase):
+    def setUp(self):
+        self.v1 = tuple(float(i) for i in range(0, 9))
+        self.v2 = tuple(2*i for i in self.v1)
+        self.v_sqr = tuple(math.pow(i, 2) for i in self.v1)
+        self.a = bullet.btMatrix3x3()
+        self.b = bullet.btMatrix3x3()
+        self.c = bullet.btMatrix3x3()
+        self.d = bullet.btMatrix3x3()
+
+    def test_constructors(self):
+        self.a = bullet.btMatrix3x3(bullet.btQuaternion.identity)
+        self.b = bullet.btMatrix3x3(self.a)
+        self.c = bullet.btMatrix3x3(0, 0, 0,
+                                    0, 0, 0,
+                                    1, 1, 1)
+
+    def test_str(self):
+        self.a = bullet.btMatrix3x3(0, 0, 0,
+                                    0, 0, 0,
+                                    0, 0, 0)
+        self.b = bullet.btMatrix3x3(self.a)
+        s = str(self.a)
+        t = repr(self.b)
+        e = "((0, 0, 0), (0, 0, 0), (0, 0, 0))"
+        self.assertEqual(s, t)
+        self.assertEqual(s, e)
+
+    def test_index(self):
+        v = [float(i) for i in range(0, 9)]
+        self.a = bullet.btMatrix3x3(*tuple(v))
+        self.assertEqual(self.a[0], bullet.btVector3(*tuple(v[:3])))
+        self.assertEqual(self.a[1], bullet.btVector3(*tuple(v[3:-3])))
+        w = [4, 5, 6]
+        self.a[0] = bullet.btVector3(*tuple(w))
+        self.assertEqual(self.a[0], bullet.btVector3(*tuple(w)))
+
+    def test_eq(self):
+        v = tuple(float(i) for i in range(0, 9))
+        self.a = bullet.btMatrix3x3(*v)
+        self.b = bullet.btMatrix3x3(*tuple(reversed(v)))
+        self.assertFalse(self.a == self.b)
+
+    def test_add(self):
+        self.a = bullet.btMatrix3x3(*self.v1)
+        self.b = bullet.btMatrix3x3(self.a)
+        self.c = self.a + self.b
+        self.a += self.b
+        self.d = bullet.btMatrix3x3(*self.v2)
+        self.assertEqual(self.a, self.c)
+        self.assertEqual(self.a, self.d)
+
+    def test_mul(self):
+        self.a = bullet.btMatrix3x3(*self.v1)
+        self.d = self.a * self.a
+        self.a *= self.a
+        self.assertEqual(self.a, self.d)
+
+    def test_sub(self):
+        self.a = bullet.btMatrix3x3(*self.v1)
+        self.b = bullet.btMatrix3x3(*self.v2)
+        self.c = self.b - self.a
+        self.b -= self.a
+        self.assertEqual(self.a, self.b)
+        self.assertEqual(self.b, self.c)
+
+    def tearDown(self):
+        del self.a
+        del self.b
+        del self.c
+        del self.d
+        del self.v1
+        del self.v2

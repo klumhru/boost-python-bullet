23a368b8f27fa8dacc3cf2388931e57c1478b723
Högni Gylfason <klumhru@gmail.com>
2013-12-16 18:43:38 +0000

    Transform implemented with tests

--- bullet/src/boost/btBoostLinearMathTransform.hpp
+++ bullet/src/boost/btBoostLinearMathTransform.hpp
@@ -13,9 +13,14 @@ using namespace std;
 
 ostream& operator<<(ostream& os, const btTransform& t)
 {
-    os << "((" << t.getBasis() << "), (" << t.getOrigin() << "))";
+    os << "basis" << t.getBasis() << ", origin" << t.getOrigin() << "";
     return os;
 }
+
+const btMatrix3x3& (btTransform::*get_basis)() const = &btTransform::getBasis;
+btMatrix3x3& (btTransform::*get_basis_ref)() = &btTransform::getBasis;
+const btVector3& (btTransform::*get_origin)() const = &btTransform::getOrigin;
+btVector3& (btTransform::*get_origin_ref)() = &btTransform::getOrigin;
 
 void defineTransform()
 {
@@ -26,10 +31,35 @@ void defineTransform()
         .def(init<const btMatrix3x3&>())
         .def(init<const btMatrix3x3&, const btVector3&>())
         // String ops
-        // Operators
-        .def(self == other<btTransform>())
         .def(self_ns::str(self_ns::self))
         .def(self_ns::repr(self_ns::self))
+        // Operators
+        .def(self == other<btTransform>())
+        .def(self * other<btVector3>())
+        .def(self * other<btTransform>())
+        .def(self *= other<btTransform>())
+        // Member functions
+        .def("mult", &btTransform::mult)
+        .def("get_basis", get_basis,
+             return_value_policy<copy_const_reference>())
+        .def("set_basis", &btTransform::setBasis)
+        .def("get_rotation", &btTransform::getRotation)
+        .def("set_rotation", &btTransform::setRotation)
+        .def("get_origin", get_origin,
+             return_value_policy<copy_const_reference>())
+        .def("set_origin", &btTransform::setOrigin)
+        // Properties
+        .add_property("basis", make_function(get_basis_ref,
+                      return_internal_reference<>()))
+        .add_property("origin", make_function(get_origin_ref,
+                      return_internal_reference<>()))
+        .add_static_property("identity",
+            make_function(btTransform::getIdentity,
+                          return_value_policy<copy_const_reference>()))
+        .def("set_identity", &btTransform::setIdentity)
+        .def("inverse", &btTransform::inverse)
+        .def("inv_xform", &btTransform::invXform)
+        .def("inverse_times", &btTransform::inverseTimes)
 
         ;
 }
--- linear_math_tests/test_transform.py
+++ linear_math_tests/test_transform.py
@@ -13,14 +13,16 @@ import bullet
 
 class TestTransform(unittest.TestCase):
     def setUp(self):
+        self.v1 = tuple(float(i) for i in range(0, 9))
+        self.vec = bullet.btVector3(0, 0, 0)
+        self.q = bullet.btQuaternion.identity
+        self.m = bullet.btMatrix3x3(*self.v1)
+
         self.a = bullet.btTransform()
         self.b = bullet.btTransform()
         self.c = bullet.btTransform()
         self.d = bullet.btTransform()
         self.e = bullet.btTransform()
-        self.v1 = tuple(float(i) for i in range(0, 9))
-        self.vec = bullet.btVector3(0, 0, 0)
-        self.q = bullet.btQuaternion.identity
 
     def test_ctors(self):
         self.a = bullet.btTransform(bullet.btMatrix3x3(*self.v1))
@@ -35,6 +37,78 @@ class TestTransform(unittest.TestCase):
         self.a = bullet.btTransform(self.q)
         self.b = bullet.btTransform(self.q)
         self.assertEqual(self.a, self.b)
+
+    def test_mult(self):
+        self.a = bullet.btTransform(self.q)
+        self.b = bullet.btTransform(bullet.btQuaternion(1, 1, 0, 0))
+        self.c = bullet.btTransform(self.q)
+        self.c.mult(self.a, self.b)
+        self.assertFalse(self.c == self.a)
+
+    def test_basis(self):
+        self.a = bullet.btTransform(self.m)
+        self.assertEqual(self.a.get_basis(), self.m)
+        w = (4, 5, 6)
+        self.a.basis[0] = bullet.btVector3(*w)
+        self.assertFalse(self.a.get_basis() == self.m)
+        self.a.set_basis(self.m)
+        self.assertEqual(self.a.get_basis(), self.m)
+
+    def test_rotation(self):
+        self.a = bullet.btTransform(self.q)
+        self.assertEqual(self.a.get_rotation(), self.q)
+        self.q = bullet.btQuaternion(0, 1, 1, 0)
+        self.assertNotEqual(self.q, self.a.get_rotation())
+        self.a.set_rotation(self.q)
+        print(self.q.normalized(), self.a.get_rotation())
+        # Hack to bypass numeric imprecision
+        # TODO: Extend test case to implement assertAlmostEqual with matrices
+        self.assertTrue(str(self.q.normalized()) == str(self.a.get_rotation()))
+
+    def test_origin(self):
+        self.a = bullet.btTransform(self.q)
+        self.b = bullet.btTransform(self.q)
+        self.vec = self.a.get_origin()
+        self.assertEqual(self.vec, self.a.get_origin())
+        self.assertEqual(self.a.get_origin(), bullet.btVector3(0, 0, 0))
+        self.a.origin.set_value(1, 0, 0)
+        self.assertEqual(self.a.get_origin(), bullet.btVector3(1, 0, 0))
+        self.a.set_origin(bullet.btVector3(0, 1, 0))
+        self.assertEqual(self.a.get_origin(), bullet.btVector3(0, 1, 0))
+
+    def test_identit(self):
+        self.a = bullet.btTransform.identity
+        self.b = bullet.btTransform(bullet.btQuaternion(1, 0, 0, 0),
+                                    bullet.btVector3(0, 1, 0))
+        self.assertEqual(self.a.get_rotation(), bullet.btQuaternion.identity)
+        self.assertEqual(self.a.get_origin(), self.vec)
+        print(self.b.get_origin(), self.vec)
+        self.assertFalse(self.b.get_origin() == self.vec)
+        self.b.set_identity()
+        self.assertEqual(self.a, self.b)
+        self.assertEqual(bullet.btTransform.identity, self.a)
+
+    def test_inverse(self):
+        self.a = bullet.btTransform(self.q, bullet.btVector3(0, 0, 1))
+        self.b = bullet.btTransform(self.q, bullet.btVector3(0, 0, -1))
+        self.c = self.a.inverse()
+        self.d = bullet.btTransform(bullet.btQuaternion.identity,
+                                    bullet.btVector3(1, 0, 0))
+        self.assertEqual(self.b, self.c)
+        self.vec = self.d.inv_xform(bullet.btVector3(0, 1, 0))
+        self.assertEqual(self.vec, bullet.btVector3(-1, 1, 0))
+        self.e = self.b.inverse_times(self.a)
+        self.assertEqual(self.e, bullet.btTransform(
+                         bullet.btQuaternion.identity,
+                         bullet.btVector3(0, 0, 2)))
+
+    def test_mul(self):
+        self.a = bullet.btTransform.identity
+        self.b = bullet.btTransform.identity
+        self.a.set_origin(bullet.btVector3(1, 1, 1))
+        self.c = self.a * self.b
+        self.a *= self.b
+        self.assertEqual(self.c, self.a)
 
     def tearDown(self):
         del self.a

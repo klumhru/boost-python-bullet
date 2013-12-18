f2a0c0c7329087421f6d3c237d2bb5f9633d180c
Högni Gylfason <klumhru@gmail.com>
2013-12-18 17:34:14 +0000

    Add some basic tests for assignment. Note that slicing is not supported

--- linear_math_tests/test_alignedobjectarray.py
+++ linear_math_tests/test_alignedobjectarray.py
@@ -13,7 +13,23 @@ import bullet
 
 class ClassTestName(unittest.TestCase):
     def setUp(self):
-        pass
+        self.a = bullet.btVector3Array()
+        for i in range(10):
+            self.a.append(bullet.btVector3(i, i+1, i+2))
+        self.b = bullet.btVector3Array()
+        for i in range(10, 20):
+            self.b.append(bullet.btVector3(i, i+1, i+2))
+
+    def test_assignment(self):
+        self.a[0] = bullet.btVector3(21, 22, 23)
+        self.assertEqual(self.a[0],
+                         bullet.btVector3(21, 22, 23))
+
+        def _slice():
+            self.a[0:3] = bullet.btVector3()
+
+        self.assertRaises(RuntimeError, _slice)
 
     def tearDown(self):
-        pass
+        del self.a
+        del self.b

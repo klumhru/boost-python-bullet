19d63177f932bf5d84e8cd1d5f84ae69d83832e8
Högni Gylfason <klumhru@gmail.com>
2013-12-19 13:49:28 +0000

    btTransformUtil complete

--- bullet/src/boost/btBoostLinearMathTransformUtil.hpp
+++ bullet/src/boost/btBoostLinearMathTransformUtil.hpp
@@ -5,11 +5,45 @@
 #include <boost/python.hpp>
 #include <LinearMath/btTransformUtil.h>
 
+using namespace boost::python;
+
+btScalar
+calculate_diff_axis_angle_quaternion(const btQuaternion& orn0,const btQuaternion& orn1,btVector3& axis)
+{
+    btScalar angle;
+    btTransformUtil::calculateDiffAxisAngleQuaternion(
+        orn0, orn1, axis, angle
+    );
+    return angle;
+}
+
+btScalar
+calculate_diff_axis_angle(const btTransform& t1,
+                          const btTransform& t2,
+                          btVector3& axis)
+{
+    btScalar angle;
+    btTransformUtil::calculateDiffAxisAngle(t1, t2, axis, angle);
+    return angle;
+}
+
 void defineTransformUtil()
 {
     class_<btTransformUtil>("btTransformUtil")
         .def("integrate_transform", btTransformUtil::integrateTransform)
-        .staticmethod("integrate_transform");
+        .staticmethod("integrate_transform")
+        .def("calculate_velocity_quaternion",
+             btTransformUtil::calculateVelocityQuaternion)
+        .staticmethod("calculate_velocity_quaternion")
+        .def("calculate_diff_axis_angle_quaternion",
+             calculate_diff_axis_angle_quaternion)
+        .staticmethod("calculate_diff_axis_angle_quaternion")
+        .def("calculate_velocity", btTransformUtil::calculateVelocity)
+        .staticmethod("calculate_velocity")
+        .def("calculate_diff_axis_angle",
+             calculate_diff_axis_angle)
+        .staticmethod("calculate_diff_axis_angle")
+    ;
 }
 
 #endif // _btBoostLinearMathTransformUtil_hpp
--- linear_math_tests/test_transformutil.py
+++ linear_math_tests/test_transformutil.py
@@ -13,26 +13,85 @@ import bullet
 
 class ClassTestName(unittest.TestCase):
     def setUp(self):
-        self.q = bullet.btQuaternion.identity
-        self.v = bullet.btVector3(0, 0, 0)
-        self.a = bullet.btTransform.identity
-        self.b = bullet.btTransform.identity
+        self.q1 = bullet.btQuaternion.identity
+        self.q2 = bullet.btQuaternion.identity
+        self.v1 = bullet.btVector3(0, 0, 0)
+        self.v2 = bullet.btVector3(0, 0, 10)
+        self.t1 = bullet.btTransform.identity
+        self.t2 = bullet.btTransform.identity
+        self.step = 1.0/60
 
     def test_integrate_transform(self):
         bullet.btTransformUtil.integrate_transform(
-            bullet.btTransform.identity,
-            bullet.btVector3(0, 0, 10),
-            bullet.btVector3(0, 0, 0),
+            self.t2,
+            self.v2,
+            self.v1,
             1.0/60,
-            self.a  # out param
+            self.t1  # out param
         )
-        self.b = bullet.btTransform.identity
-        self.b.set_origin(bullet.btVector3(0, 0, 10*(1.0/60)))
-        self.assertEqual(self.a,
-                         self.b)
+        self.t2 = bullet.btTransform.identity
+        self.t2.set_origin(bullet.btVector3(0, 0, 10*(self.step)))
+        self.assertEqual(self.t1,
+                         self.t2)
+
+    def test_calculate_velocity_quaternion(self):
+        self.q2.set_euler(0,
+                          bullet.btRadians(90),
+                          0)
+        lin_vel = bullet.btVector3(0, 0, 0)
+        ang_vel = bullet.btVector3(0, 0, 0)
+        bullet.btTransformUtil.calculate_velocity_quaternion(
+            self.v1,
+            self.v2*self.step,
+            self.q1,
+            self.q2,
+            self.step,
+            lin_vel,  # out param
+            ang_vel   # out param
+        )
+        print('lin_vel', lin_vel)
+        print('ang_vel', ang_vel)
+        print('ang_vel.x degrees', bullet.btRadians(ang_vel.x))
+        self.assertEqual(lin_vel, self.v2)
+
+    def test_calculate_diff_axis_angle_quaternion(self):
+        self.q2.set_rotation(bullet.btVector3(0, 0, -1),
+                             bullet.btRadians(90))
+        angle = bullet.btRadians(0.0)
+        angle = bullet.btTransformUtil.calculate_diff_axis_angle_quaternion(
+            self.q1,
+            self.q2,
+            self.v1
+        )
+        self.assertFalse(angle == 0.0)
+        self.assertEqual(angle, bullet.btRadians(90))
+
+    def test_calculate_velocity(self):
+        self.t2.set_origin(self.v2)
+        bullet.btTransformUtil.calculate_velocity(
+            self.t1, self.t2, self.step,
+            self.v1,  # linVel
+            self.v2   # angVel
+        )
+        self.assertEqual(self.v1, self.t2.origin/self.step)
+        self.assertEqual(self.v2, bullet.btVector3(0, 0, 0))
+
+    def test_calculate_diff_axis_angle(self):
+        self.v1 = bullet.btVector3(0, 0, -1)
+        self.q1 = bullet.btQuaternion(self.v1,
+                                      bullet.btRadians(90))
+        self.t2.set_rotation(self.q1)
+        angle = bullet.btTransformUtil.calculate_diff_axis_angle(
+            self.t1, self.t2,
+            self.v2  # out val
+        )
+        self.assertEqual(angle, bullet.btRadians(90))
+        self.assertEqual(self.v1, self.v2)
 
     def tearDown(self):
-        del self.a
-        del self.b
-        del self.q
-        del self.v
+        del self.t1
+        del self.t2
+        del self.q1
+        del self.q2
+        del self.v1
+        del self.v2

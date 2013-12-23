408d83c96744cfbce2e6eac321c11df4b476bc2a
Högni Gylfason <klumhru@gmail.com>
2013-12-23 11:06:25 +0000

    boilerplate for the most basic world

---
+++ bullet_cpp/src/btBoost/btBoostDynamicsDiscreteDynamicsWorld.hpp
@@ -0,0 +1,14 @@
+// File: btBoostDynamicsDiscreteDynamicsWorld.hpp
+#ifndef _btBoostDynamicsDiscreteDynamicsWorld_hpp
+#define _btBoostDynamicsDiscreteDynamicsWorld_hpp
+
+#include <boost/python.hpp>
+#include <btBulletDynamicsCommon.h>
+
+using namespace boost::python;
+
+void defineDiscreteDynamicsWorld()
+{
+}
+
+#endif // _btBoostDynamicsDiscreteDynamicsWorld_hpp

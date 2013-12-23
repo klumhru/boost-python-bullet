950217ca67376674460a055d3e298c560fa5e584
Högni Gylfason <klumhru@gmail.com>
2013-12-23 11:07:56 +0000

    boilerplate for world tests

---
+++ test_collision/test_worlds.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+test_collision.test_worlds
+"""
+from __future__ import unicode_literals, print_function, absolute_import
+
+import unittest
+
+import bullet
+
+
+class DiscreteDynamicsWorldTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_ctor(self):
+        pass
+
+    def tearDown(self):
+        pass

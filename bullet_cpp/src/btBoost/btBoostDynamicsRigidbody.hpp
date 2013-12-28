ea33ab0dd44b5ce9cd74f0766a4a75b1b71fe937
Högni Gylfason <klumhru@gmail.com>
2013-12-28 17:08:26 +0000

    scaffolding for rigidbody - waiting until shapes are done

---
+++ bullet_cpp/src/btBoost/btBoostDynamicsRigidbody.hpp
@@ -0,0 +1,13 @@
+// File: btBoostDynamicsRigidbody.hpp
+#ifndef _btBoostDynamicsRigidbody_hpp
+#define _btBoostDynamicsRigidbody_hpp
+
+#include <btBulletDynamicsCommon.h>
+#include <boost/python.hpp>
+
+void defineRigidbody()
+{
+
+}
+
+#endif // _btBoostDynamicsRigidbody_hpp

6e56d38349947c7128e2d197b20d8aae30eb2ea1
Högni Gylfason <klumhru@gmail.com>
2014-01-02 16:44:20 +0000

    Needed array for scalars

--- bullet_cpp/src/btBoost/btBoostLinearMathAlignedObjectArray.hpp
+++ bullet_cpp/src/btBoost/btBoostLinearMathAlignedObjectArray.hpp
@@ -183,13 +183,22 @@ public:
 };
 
 typedef btAlignedObjectArray<int> btIntArray;
+typedef btAlignedObjectArray<btScalar> btScalarArray;
 
 void definePrimitiveArrays()
 {
+    // TODO: Needs tests
     class_<btIntArray>("btIntArray")
         .def(init<btIntArray>())
         .def(bt_ref_index_suite<btIntArray>())
         .def("append", &btIntArray::push_back)
+    ;
+
+    // TODO: Needs tests
+    class_<btScalarArray>("btScalarArray")
+        .def(init<btScalarArray>())
+        .def(bt_ref_index_suite<btScalarArray>())
+        .def("append", &btScalarArray::push_back)
     ;
 }
 

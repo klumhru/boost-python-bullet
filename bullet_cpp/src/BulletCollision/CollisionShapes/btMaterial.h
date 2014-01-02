daeeb4a71ce9ea2bdbcd8ef1f31e8f66dbd7f563
Högni Gylfason <klumhru@gmail.com>
2014-01-02 16:53:53 +0000

    Explicitly include btScalar

--- bullet_cpp/src/BulletCollision/CollisionShapes/btMaterial.h
+++ bullet_cpp/src/BulletCollision/CollisionShapes/btMaterial.h
@@ -4,8 +4,8 @@ Copyright (c) 2003-2009 Erwin Coumans  http://bulletphysics.org
 
 This software is provided 'as-is', without any express or implied warranty.
 In no event will the authors be held liable for any damages arising from the use of this software.
-Permission is granted to anyone to use this software for any purpose, 
-including commercial applications, and to alter it and redistribute it freely, 
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it freely,
 subject to the following restrictions:
 
 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
@@ -17,6 +17,8 @@ subject to the following restrictions:
 
 #ifndef BT_MATERIAL_H
 #define BT_MATERIAL_H
+
+#include <LinearMath/btScalar.h>
 
 // Material class to be used by btMultimaterialTriangleMeshShape to store triangle properties
 class btMaterial

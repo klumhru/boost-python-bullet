f9be6d6b330b1dc314ca2088ff5683d6cd9ef1c1
Högni Gylfason <klumhru@gmail.com>
2014-01-03 09:50:22 +0000

    Added getters for the two lists for wrapping. Wrapping raw pointers is a pain

--- bullet_cpp/src/BulletCollision/CollisionShapes/btShapeHull.h
+++ bullet_cpp/src/BulletCollision/CollisionShapes/btShapeHull.h
@@ -4,8 +4,8 @@ Copyright (c) 2003-2009 Erwin Coumans  http://bulletphysics.org
 
 This software is provided 'as-is', without any express or implied warranty.
 In no event will the authors be held liable for any damages arising from the use of this software.
-Permission is granted to anyone to use this software for any purpose, 
-including commercial applications, and to alter it and redistribute it freely, 
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it freely,
 subject to the following restrictions:
 
 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
@@ -38,7 +38,7 @@ protected:
 
 public:
 	BT_DECLARE_ALIGNED_ALLOCATOR();
-	
+
 	btShapeHull (const btConvexShape* shape);
 	~btShapeHull ();
 
@@ -55,6 +55,15 @@ public:
 	const unsigned int* getIndexPointer() const
 	{
 		return &m_indices[0];
+	}
+	// Added Högni Gylfason 03.01.2014
+	const btAlignedObjectArray<btVector3>& getVertexList() const
+	{
+		return m_vertices;
+	}
+	const btAlignedObjectArray<unsigned int>& getIndexList() const
+	{
+		return m_indices;
 	}
 };
 

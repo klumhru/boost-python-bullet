9c28b0813ef1ba88e9335dd0a61167c218b62636
Högni Gylfason <klumhru@gmail.com>
2014-01-20 14:16:54 +0000

    Added thin wrappers for data interchange files
    These are pure abstract classes and inheritors that need to be further worked on

---
+++ bullet_cpp/src/btBoost/btBoostCollisionOverLappingPairCache.hpp
@@ -0,0 +1,44 @@
+// File: btBoostCollisionOverLappingPairCache.hpp
+#ifndef _btBoostCollisionOverLappingPairCache_hpp
+#define _btBoostCollisionOverLappingPairCache_hpp
+
+#include <boost/python.hpp>
+#include <boost/shared_ptr.hpp>
+#include <btBulletDynamicsCommon.h>
+#include "btBoostCollisionWorldWrappers.hpp"
+
+// Very thin wrappers for data interchange classes and callbacks
+// TODO: Implement the wrappers fully
+void defineOverlappingPairCaches()
+{
+    class_<btOverlapCallback, boost::noncopyable>
+        ("btOverlapCallback", no_init)
+    ;
+
+    class_<btOverlapFilterCallback, boost::noncopyable>
+        ("btOverlapFilterCallback", no_init)
+    ;
+
+    class_<btOverlappingPairCallback, boost::noncopyable>
+        ("btOverlappingPairCallback", no_init)
+    ;
+
+    class_<btOverlappingPairCache, bases<btOverlappingPairCallback>,
+           boost::noncopyable>
+        ("btOverlappingPairCache", no_init)
+    ;
+
+    class_<btHashedOverlappingPairCache, bases<btOverlappingPairCache> >
+        ("btHashedOverlappingPairCache")
+    ;
+
+    class_<btSortedOverlappingPairCache, bases<btOverlappingPairCache> >
+        ("btSortedOverlappingPairCache")
+    ;
+
+    class_<btNullPairCache, bases<btOverlappingPairCache> >
+        ("btNullPairCache")
+    ;
+}
+
+#endif // _btBoostCollisionOverLappingPairCache_hpp
